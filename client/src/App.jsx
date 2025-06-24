35d040b7f2b71533a9e3dbd115f6f0700727d718
Navneet Vishwakrma <rajvl132011@gmail.com>
2025-06-24 21:35:56 +0530

    intial commit  AA

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,11 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
-import Home from './pages/Home.jsx';
-import About from './pages/About.jsx';
-import BookAppointment from './pages/BookAppointment.jsx';
-import Login from './pages/Login.jsx';
-import Profile from './pages/Profile.jsx';
-import Signup from './pages/Signup.jsx';
-import Trainers from './pages/Trainers.jsx';
+import React from 'react'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/book" element={<BookAppointment />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/trainers" element={<Trainers />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
-};
+    <div>
+      
+    </div>
+  )
+}
 
-export default App;
+export default App
